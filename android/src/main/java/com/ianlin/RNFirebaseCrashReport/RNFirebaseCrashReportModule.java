ec0b0e025ca8fd0a8d30c1f6c1cf34458820053b
ramkrishna samanta <ramkrishna.samanta@agnicient.com>
2018-07-27 15:40:31 +0530

    Update RNFirebaseCrashReportModule.java

--- android/src/main/java/com/ianlin/RNFirebaseCrashReport/RNFirebaseCrashReportModule.java
+++ android/src/main/java/com/ianlin/RNFirebaseCrashReport/RNFirebaseCrashReportModule.java
@@ -1,14 +1,18 @@
 package com.ianlin.RNFirebaseCrashReport;
 
+import android.app.Activity;
+import android.os.Bundle;
 import com.facebook.react.bridge.LifecycleEventListener;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-
+import com.google.firebase.analytics.FirebaseAnalytics;
 import com.google.firebase.crash.FirebaseCrash;
+
 
 public class RNFirebaseCrashReportModule extends ReactContextBaseJavaModule implements LifecycleEventListener {
     private final static String TAG = RNFirebaseCrashReportModule.class.getCanonicalName();
+    private FirebaseAnalytics mFirebaseAnalytics = FirebaseAnalytics.getInstance(getReactApplicationContext());
 
     public RNFirebaseCrashReportModule(ReactApplicationContext reactContext) {
         super(reactContext);
@@ -33,6 +37,32 @@ public class RNFirebaseCrashReportModule extends ReactContextBaseJavaModule impl
     @ReactMethod
     public void report(String message) {
         FirebaseCrash.report(new Exception(message));
+    }
+
+    /**
+     * @param eventName screen         SEARCH
+     * @param key       screen_name    SEARCH_TERM
+     * @param value     Home           010121/rice
+     */
+
+    @ReactMethod
+    public void setCurrentScreen(final String eventName, final String key, final String value) {
+        if (!eventName.equals("") && !key.equals("") && !value.equals("")) {
+            Bundle bundle = new Bundle();
+            bundle.putString(key, value);
+            mFirebaseAnalytics.logEvent(eventName, bundle);
+        } else if (!eventName.equals("")) {
+            final Activity activity = getCurrentActivity();
+            if (activity != null) {
+                // needs to be run on main thread
+                activity.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        FirebaseAnalytics.getInstance(getReactApplicationContext()).setCurrentScreen(activity, eventName, null);
+                    }
+                });
+            }
+        }
     }
 
     @Override
