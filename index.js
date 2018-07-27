70782197e29ccdc4ebf77093241f159bb765fc45
ramkrishna samanta <ramkrishna.samanta@agnicient.com>
2018-07-27 15:29:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
 'use strict';
 
-var { Platform, NativeModules } = require('react-native');
+var {Platform, NativeModules} = require('react-native');
 const _FirebaseCrashReport = NativeModules.RNFirebaseCrashReport;
 
 export default class FirebaseCrashReport {
 
-    static log(message: string) {
-        _FirebaseCrashReport.log(message);
-    }
+	static log(message: string) {
+		_FirebaseCrashReport.log(message);
+	}
 
-    static logcat(message: string, level: number = null, tag: string = null) {
-        if (Platform.OS === 'ios') {
-            _FirebaseCrashReport.logcat(message);
-        } else if (Platform.OS === 'android') {
-            level = (typeof level !== 'number') ? 3 : level; // --- default level: 3 - DEBUG
-            tag = (typeof tag !== 'string') ? 'RNFirebaseCrashReport' : tag;
-            _FirebaseCrashReport.logcat(level, tag, message);
-        }
-    }
+	static logcat(message: string, level: number = null, tag: string = null) {
+		if (Platform.OS === 'ios') {
+			_FirebaseCrashReport.logcat(message);
+		}
+		else if (Platform.OS === 'android') {
+			level = (typeof level !== 'number') ? 3 : level; // --- default level: 3 - DEBUG
+			tag = (typeof tag !== 'string') ? 'RNFirebaseCrashReport' : tag;
+			_FirebaseCrashReport.logcat(level, tag, message);
+		}
+	}
 
-    static report(message) {
-        _FirebaseCrashReport.report(message);
-    }
+	static report(message) {
+		_FirebaseCrashReport.report(message);
+	}
+
+	static setCurrentScreen(eventName = '', key = '', value = '') {
+		_FirebaseCrashReport.setCurrentScreen(eventName, key, value);
+	}
 }
